0e2a7aafd4ef6a41f4af395695a2c82d9db0f9d3
RobertJDevOP <rcjimenez35@gmail.com>
2022-02-23 16:03:45 -0500

    chore: add vue js and webpack to project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+import { createApp } from "vue";
+import Example from './components/Example'
+
+
+createApp({})
+    .component('example',require("./components/Example.vue").default)
+    .mount('#app')
---
+++ resources/js/components/Example.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Test component vue 3
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
