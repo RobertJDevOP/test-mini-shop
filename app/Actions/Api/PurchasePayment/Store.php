d76203fcbd456a787c73d47275e6fd124aa33d04
RobertJDevOP <rcjimenez35@gmail.com>
2022-03-07 13:58:11 -0500

    refactor : refactor cyclomatic complexity in status of wallet payment

--- app/Actions/Api/PurchasePayment/Store.php
+++ app/Actions/Api/PurchasePayment/Store.php
@@ -2,20 +2,14 @@
 
 namespace App\Actions\Api\PurchasePayment;
 
+use App\Constants\PlacetopayStatusResponses;
 use App\Models\PurchasePayment;
-use Illuminate\Support\Facades\Log;
 
 class Store
 {
     public static function execute(array $data,int $purchaseOrderId): void
     {
-        if('OK' === $data['status']['status']){
-            $statusTransaction = 'CREATED';
-        }else if('APPROVED' === $data['status']['status']) {
-            $statusTransaction = 'PAYED';
-        }else{
-            $statusTransaction = $data['status']['status'];
-        }
+        $statusTransaction = PlacetopayStatusResponses::getStatus($data['status']['status']);
 
         $purchasePayment = new PurchasePayment();
         $purchasePayment->id_purchase_order = $purchaseOrderId;
--- app/Constants/PlacetopayStatusResponses.php
+++ app/Constants/PlacetopayStatusResponses.php
@@ -4,6 +4,7 @@ namespace App\Constants;
 
 class PlacetopayStatusResponses
 {
+    private const STATUSES = ['OK' => 'CREATED','REJECTED' => 'FAILED','APPROVED' => 'PAYED'];
     public const OK = 'OK';
     public const FAILED = 'FAILED';
     public const APPROVED = 'APPROVED';
@@ -13,4 +14,9 @@ class PlacetopayStatusResponses
     public const REFUNDED = 'REFUNDED';
     public const ERROR = 'ERROR';
     public const CREATED = 'CREATED';
+
+    public static function getStatus(string $status): string
+    {
+        return self::STATUSES[$status] ?? 'FAILED';
+    }
 }
